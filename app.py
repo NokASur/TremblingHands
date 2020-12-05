66ff1d591aa003ce3484ce538b3a8db6232643ef
asurov2022 <asurov2017@litsey2.ru>
2020-12-05 11:49:38 +0300

    shame

--- app.py
+++ app.py
@@ -1,10 +1,23 @@
 from flask import Flask
+import random as rnd
 
 app = Flask(__name__)
 
 
-@app.route('/')
-@app.route('/haba')
+@app.route('/task1/random/')
 def hello_world():
-    text = 'Hello, Haba!\nHello, Arsen!\nHello, Karim!'
+    text = "Haba's mark is " + str(rnd.randint(1, 5))
     return f'<pre>{text}</pre>'
+
+
+@app.route('/task1/i_will_not/')
+def hello_world1():
+    text = "<li>I will not waste time</li>\n" * 100
+    return f'<ul>{text}</ul>'
+
+
+@app.route('/')
+def hello_world2():
+    text = "<li><a href='/task1/random/'>/task1/random/</a></li>\n" \
+           "<li><a href='/task1/i_will_not/'>/task1/i_will_not/</a></li>"
+    return f'<ul>{text}</ul>'
