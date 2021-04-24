1b69d160eaec018aa35cb9523a19c31691a4ebed
SASergeevich <75212119+SASergeevich@users.noreply.github.com>
2021-04-24 10:20:29 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
-from flask import Flask, render_template, request
-import os
+from flask import Flask, render_template, request, url_for, session,redirect
 import sys
 from sqlalchemy import Column, ForeignKey, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
-
+from werkzeug.security import generate_password_hash, check_password_hash
+from werkzeug.middleware.proxy_fix import ProxyFix
+import json, os, psycopg2, smtplib
+from datetime import datetime
 
 database_url = os.environ['DATABASE_URL'].split(':')
 database_url = database_url[0] + "ql:" + ":".join(database_url[1:])
@@ -15,6 +17,9 @@ engine = create_engine(database_url)
 Base = declarative_base()
 Base.metadata.create_all(engine)
 chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
+conn = psycopg2.connect(database_url)
+cur = conn.cursor()
 
 
 class stupeeed_users(Base):
@@ -34,11 +39,24 @@ def index1():
     return "balls"
 
 
-
-@app.route("/task5/sign-up", methods=['GET', 'POST'])
+@app.route("/task5/sign-in", methods=['GET', 'POST'])
 def index():
     if request.method == 'GET':
         return render_template("sign_in.html", CSK=os.environ['RECAPTCHA_SITE_KEY'])
     email = request.form['email']
     password = request.form['password']
-    
+    cur.execute(f"SELECT COUNT(*) FROM users WHERE email = '{email}'")
+    count = cur.fetchall()[0][0]
+    if count == 0:
+        return render_template("sign-in.html", error=True)
+    cur.execute(f"SELECT password FROM users WHERE email = '{email}'")
+    password_hash = cur.fetchall()[0][0]
+    if not check_password_hash(password_hash, password):
+        return render_template("sign-in.html", error=True)
+    time = datetime.now()
+    session['email'] = email
+    cur.execute(f"INSERT INTO ips (email, ip, time) VALUES ('{email}', '{request.remote_addr}', '{time}')")
+    conn.commit()
+    session['auth'] = 'logged'
+    return redirect(url_for('account'))
+
